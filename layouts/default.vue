32c9a6d420c310efe7900d84a5b60083b33bbcda
David Robertson <85001306+daver987@users.noreply.github.com>
2023-04-29 07:57:32 -0400

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { useToast, TwFeather, TwToast } from "vue3-tailwind";
+import { useThemeStore } from "~~/store/theme";
+const themeStore = useThemeStore();
+
+useHead({
+  link: [{ rel: "icon", type: "image/x-icon", href: "/icon.png" }],
+  htmlAttrs: {
+    lang: "en",
+  },
+  bodyAttrs: {
+    class: themeStore.appTheme,
+    'data-theme': themeStore.appTheme
+  }
+});
+const { toasts } = useToast();
+</script>
+
+<template>
+  <div>
+    <TwToast position="bottom-right" :toasts="toasts" />
+    <div>
+      <LayoutFloating />
+      <LayoutHeader />
+      <div class="py-4 px-0 md:px-1">
+        <div
+          class="flex min-h-screen text-sky-900 dark:text-gray-200 bg-opacity-40 bg-gray-900 rounded-3xl shadow-lg"
+        >
+          <LayoutSidebar class="h-full" />
+          <div
+            class="w-full md:w-[calc(100%-8rem)] lg:w-[calc(100%-16rem)] sm:rounded-l-none"
+          >
+            <div class="p-4 bg-gray-100 dark:bg-gray-900 h-full rounded-3xl">
+              <slot />
+            </div>
+          </div>
+        </div>
+        <div class="px-6 md:px-11">
+          <div
+            class="w-full bg-gray-900 shadow-xl bg-opacity-60 rounded-b-2xl h-4"
+          ></div>
+        </div>
+      </div>
+      <div
+        class="text-center flex items-center gap-1 justify-center text-gray-200"
+      >
+        Developed with
+        <TwFeather class="text-red-500" type="heart" /> By
+        <a
+          class="text-blue-300"
+          href="https://github.com/WailanTirajoh"
+          target="_blank"
+        >
+          Wailan
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
